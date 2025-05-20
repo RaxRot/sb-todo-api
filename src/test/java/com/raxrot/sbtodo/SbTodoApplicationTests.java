a902c4c5c5720cd65e68782031a1957a3a351b5e
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-20 18:40:56 +0100

    init

---
+++ src/test/java/com/raxrot/sbtodo/SbTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.sbtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
