35b22c78e77529e720de84d088ed692cf7213148
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-22 18:30:02 +0100

    add open api definition

--- src/main/java/com/raxrot/sbtodo/SbTodoApplication.java
+++ src/main/java/com/raxrot/sbtodo/SbTodoApplication.java
@@ -1,7 +1,23 @@
 package com.raxrot.sbtodo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "TODO API",
+                version = "1.0",
+                description = "Simple Api for Todo tasks",
+                contact = @Contact(
+                        name = "RaxRot",
+                        email = "dasistperfektos@gmail.com",
+                        url = "https://github.com/RaxRot"
+                )
+        )
+)
 
 @SpringBootApplication
 public class SbTodoApplication {
