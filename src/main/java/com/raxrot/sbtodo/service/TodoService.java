e62f95eeee529b39af909e436632edd4987dede6
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-22 17:36:37 +0100

    add TodoService

---
+++ src/main/java/com/raxrot/sbtodo/service/TodoService.java
@@ -0,0 +1,13 @@
+package com.raxrot.sbtodo.service;
+
+import com.raxrot.sbtodo.dto.TodoDTO;
+
+import java.util.List;
+
+public interface TodoService {
+  TodoDTO createTodo(TodoDTO todoDTO);
+  List<TodoDTO>getAllTodos();
+  TodoDTO getTodoById(Long id);
+  TodoDTO updateTodo(Long id,TodoDTO todoDTO);
+  void deleteTodo(Long id);
+}
