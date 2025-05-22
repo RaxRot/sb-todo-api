9569acd84dabf5ad96cd377e9c6595b53d00fd26
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-05-22 17:42:11 +0100

    add GlobalExceptionHandler

---
+++ src/main/java/com/raxrot/sbtodo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.raxrot.sbtodo.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<Map<String,String>>handleApiException(ApiException e){
+        Map<String,String>errors=new HashMap<>();
+        errors.put("error",e.getMessage());
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
